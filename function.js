2ea162e430d36823a29fcf3b515a4bda8191db82
VDarshanR <darshanv1999@gmail.com>
2023-10-20 22:40:40 +0530

    User-Interaction updated

--- function.js
+++ function.js
@@ -1,144 +1,183 @@
 $(document).ready(function()
 {
     fetchFiles();
-})
+	$('#Deletemodal').modal({backdrop: 'static',keyboard: false});
+    $('#Updatemodal').modal({backdrop: 'static',keyboard: false});
+});
 
+//Upload
+function upload()
+{
+	var form_data = new FormData();
+	form_data.append("action","uploadit");
+	form_data.append("file",document.getElementById('file-upload').files[0]);
+	$.ajax ({
+		url: "upload.php", 
+		type: "POST",
+		data:  form_data,
+		contentType: false, //important to tell jQuery not to set the Content-Type header, allowing the browser to set it automatically based on the form data.
+        processData: false, //important to tell jQuery not to process the data, as jQuery tries to process it as a query string by default.
+		success: function(result)
+		{
+			var response = JSON.parse(result);
+			if (response.status === "error") {
+				$('#UploadMessage').html('<p class="text-danger">' + response.message + '</p>');
+            } else if (response.status === "success") {
+				$('#UploadMessage').html('<p class="text-success">' + response.message + '</p>');
+                setTimeout(function() {
+                    $('#Uploadmodal').find('[data-dismiss="modal"]').trigger('click');
+					$('#UploadMessage').html('');
+                }, 700);
+				$('form').trigger('reset');
+				fetchFiles();
+			}
+		}
+	});
+	$(document).on('click','#uploadbtn_close', function()
+	{
+		$('form').trigger('reset');
+		$('#UploadMessage').html('');
+	})	
+}
+
+//Fetch
 function fetchFiles()
 {
 	$.ajax({
 		type:'post',
 		url:'upload.php',
 		data:"action=fetchfiles",
-		cache:false,
-		async:true,
 		success: function(result)
 		{
 			if(result != null)
 			{
-			    showfetchFiles(JSON.parse(result));		
+				showfetchFiles(JSON.parse(result));
 			}
 		}
 	});
 }
-
 function showfetchFiles(res)
 {
-	if (res.length <= 2) { // Check if there are no files uploaded
+	if (res.length == 0) { // Check if there are no files uploaded
         $("#table").hide(); // Hide the table if no files uploaded
         return;
     }
-	var tbData = "<table class='table-bordered' width=60% style='background:#191970; color:#ffffff'>";
-	tbData  += "<tr style= 'background-color: #000000'><th>Name</th><th>Modified</th><th>Size</th><th>Delete</th><th>Edit</th></tr>";		
-	for(var i=2;i<res.length;i++) //index starts from 2 because the res array contains two items at the beginning. These two items are the root folder (..) and the current folder (.). 
+	else{
+		$("#table").show();
+	}
+	var tbData = "<table class='table-bordered' width=60%>";
+	tbData  += "<tr style= 'background-color: #000000; color: #ffffff;'><th>Name</th><th>Modified</th><th>Size</th><th>Delete</th><th>Edit</th><th>Download</th></tr>";		
+	for(var i=0;i<res.length;i++)
 	{
-		tbData +=  "<tr align=center><td>"+res[i].name+"</td><td>"+res[i].modified+"</td><td>"+res[i].size+"</td><td><i class='fas fa-trash' onclick='todelete(\""+res[i].name+"\" )' style=cursor:pointer;'></i></td><td><i class='fas fa-edit' onclick='toupdate(\""+res[i].name+"\")' style=cursor:pointer;'></i></td></tr>";
+		tbData +=  "<tr class='table-row-hover' style='background:#ffffff; color:#000000;' align=center><td>"+res[i].name+"</td><td>"+res[i].modified+"</td><td>"+res[i].size+"</td><td><i class='fas fa-trash' onclick='todelete(\""+res[i].name+"\" )' style=cursor:pointer;'></i></td><td><i class='fas fa-edit' onclick='toupdate(\""+res[i].name+"\")' style=cursor:pointer;'></i></td><td><i class='fa fa-download' onclick='downloadFile(\"" + res[i].name + "\", \"" + getFileExtension(res[i].name) + "\")' style='cursor:pointer;'></i></td></tr>";
 	}
 	tbData += "</table>";
-	$("#table").html(tbData);
+	$("#table").html(tbData); 
 }
 
-function upload(res)
-{
-	var form_data = new FormData();
-	form_data.append("action","uploadit");
-	form_data.append("file",document.getElementById('file-upload').files[0]);
-	$.ajax
-	({
-		url: "upload.php", 
-		type: "POST",
-		data:  form_data,
-		contentType: false,
-		cache: false,
-		processData:false,
-		success: function(result)
-		{
-			$('#message').html(result);
-			$('form').trigger('reset');
-			fetchFiles();
-			location.reload();
-		}
-	});
-	$(document).on('click','#btn_close', function()
-	{
-		$('form').trigger('reset');
-		$('#message').html('');
-	})	
-}
-
-function todelete(res)
-{
-	$('#Deletemodal').modal('show');
-	$("#filename1").val(res);
-	$("#hide").val(res);
-	$(document).on('click','#btn_close',function()
-	{
-		$("#Deletemodal").modal('hide');
-		$(".modal-backdrop").remove();
-	})		
-}
-function deletee()
-{
-    $.ajax({
-        url: 'upload.php',
-        method: 'post',
-        data:'action=delete&file='+$("#filename1").val(),
-        success:function(data)
-        {
-            if(JSON.parse(data).result == "success")
-            {  
-                alert("Successfully deleted.");
-                $('#Deletemodal').modal('hide');
-                $('.modal-backdrop').remove();
-                fetchFiles();
-            }
-			else{
-				alert("Can't delete file.");
-                $('#Deletemodal').modal('hide');
-				$('.modal-backdrop').remove();
-                fetchFiles();
-			}
-        }
-    })
-}
-
+//Update
 function toupdate(res)
 {
 	$('#Updatemodal').modal('show');
-	$("#filename").val(res);
+	$("#updatefilename").val(res);
 	$("#hide").val(res);
-	$(document).on('click','#btn_close',function()
-	{
-		$("#Updatemodal").modal('hide');
-		$(".modal-backdrop").remove();
-	})
 }
 function update()
 {
 	$.ajax({
 		url: 'upload.php',
 		method: 'post',
-		data:'action=update&fname='+$("#hide").val()+'&newname='+$("#filename").val(),
-		success:function(data, status, xhr)
+		data:'action=update&fname='+$("#hide").val()+'&newname='+$("#updatefilename").val(),
+		success:function(result)
         {
-			if(xhr.status === 200)
-			{
-				var response = JSON.parse(data);
-				alert(response.message);
-				$('#Updatemodal').modal('hide');
-				$('.modal-backdrop').remove();
+			var response = JSON.parse(result);
+			if (response.status === "error") {
+				 $('#UpdateConfirmationMessage').hide();
+				 $('#UpdateMessage').html('<p class="text-danger">' + response.message + '</p>');
+			} else if (response.status === "success") {
+				 $('#UpdateConfirmationMessage').hide();
+				 $('#UpdateMessage').html('<p class="text-success">' + response.message + '</p>');
+				setTimeout(function() {
+					$('#Updatemodal').modal('hide');
+					$('.modal-backdrop').remove();
+					$('#UpdateConfirmationMessage').show();
+					$("#UpdateMessage").html('');
+                }, 700);
 				fetchFiles();
 			}
-		},
-		error: function(xhr, status, error) 
-		{
-			if (xhr.status === 400) 
-			{
-				var response = JSON.parse(xhr.responseText);
-                alert(response.message);
-                $('#Updatemodal').modal('hide');
-				$('.modal-backdrop').remove();
-                fetchFiles();
-			}
 		}
-	})
+	});
+}
+$(document).on('click','#updatebtn_close',function()
+{
+	$("#Updatemodal").modal('hide');
+	$(".modal-backdrop").remove();
+	$("#UpdateMessage").html('');
+	$('#UpdateConfirmationMessage').show();
+		
+});
+
+//Delete
+function todelete(res)
+{
+	$('#Deletemodal').modal('show');
+	$("#deletefilename").val(res);
+	$("#hide").val(res);
+}
+function deletee()
+{
+    $.ajax({
+        url: 'upload.php',
+        method: 'post',
+        data:'action=delete&file='+$("#deletefilename").val(),
+        success:function(result)
+        {
+			var response = JSON.parse(result);
+            if(response.status === "success")
+            {
+				$('#DeleteConfirmationMessage').hide();
+                $("#DeleteMessage").html('<p class="text-success">' + response.message + '</p>')
+				setTimeout(function() {
+					$('#Deletemodal').modal('hide');
+                	$('.modal-backdrop').remove();
+					$('#DeleteConfirmationMessage').show();
+					$('#DeleteMessage').html('');
+				}, 700);
+                fetchFiles();
+            }
+        }
+    })
+}
+$(document).on('click','#deletebtn_close',function()
+{
+	$("#Deletemodal").modal('hide');
+	$(".modal-backdrop").remove();
+})		
+
+//Download
+function getFileExtension(filename) {
+    return filename.split('.').pop().toLowerCase();
+}
+function downloadFile(filename, extension) {
+    $.ajax({
+        type: 'POST',
+        url: 'upload.php',
+        data: {
+            action: 'download',
+            filename: filename,
+            extension: extension
+        },
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function(response, status, xhr) {
+            var contentType = xhr.getResponseHeader("content-type") || "application/octet-stream";
+            var blob = new Blob([response], { type: contentType });
+            var url = window.URL.createObjectURL(blob);
+            var link = document.createElement('a');
+            link.href = url;
+            link.download = filename;
+            link.click();
+        },
+    });
 }
--- upload.php
+++ upload.php
@@ -1,51 +1,57 @@
 <?php
-if (isset($_POST['action']))
-{   
-  if($_POST["action"] == "uploadit")
-  {
-	if(empty($_FILES['file']['tmp_name'])) 
+	$response = new stdClass();
+	if($_POST["action"] == "uploadit")
 	{
-		echo "<script>alert('Please select a file to upload.');</script>";
-	}
-	else
-	{
-		$fileTmpPath = $_FILES['file']['tmp_name']; //temporary path to the uploaded file.
-		$fileName = $_FILES['file']['name']; //name of the uploaded file.
-		$fileSize = $_FILES['file']['size']; //size of the uploaded file in bytes.
-		$fileType = $_FILES['file']['type']; //Multipurpose Internet Mail Extensions(MIME)type of the uploaded file.
-		$fileNameCmps = explode(".", $fileName); //an array created by exploding the $fileName on the "." character. This is done to get the extension of the file.
-		$fileExtension = strtolower(end($fileNameCmps)); //extension of the uploaded file in lowercase. 	
-		$uploadFileDir = './uploads/'; //directory where the file will be uploaded.
-		$dest_path = $uploadFileDir . $fileName; //set to the full path of the uploaded file, which is a combination of $uploadFileDir and $fileName.
-		$target_file = $uploadFileDir.basename($fileName); // set to the basename of the uploaded file, which is the same as $fileName.
-		$allowedFileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'pdf', 'docx');
-		
-		if (in_array($fileExtension, $allowedFileExtensions))
+		if(empty($_FILES['file']['tmp_name'])) 
 		{
-		  if(file_exists($target_file))
-		  {
-			echo "<script>alert('Already file exist.');</script>";
-		  }
-		  elseif($fileSize>50000000)
-		  {
-			echo "<script>alert('File is too large to upload.');</script>";
-		  }       
-		  elseif(move_uploaded_file($fileTmpPath, $dest_path))
-		  {
-			echo "<script>alert('File is successfully uploaded.');</script>";
-		  }
-		  else
-		  {
-			echo "<script>alert('Please make sure that the upload directory is writable by web server.');</script>";
-		  }
+			$response->status = "error";
+			$response->message =  "Please select a file to upload";
 		}
 		else
 		{
-		  echo "<script>alert('Upload valid extension.');</script>";
+			$fileTmpPath = $_FILES['file']['tmp_name']; //temporary path to the uploaded file.
+			$fileName = $_FILES['file']['name']; //name of the uploaded file.
+			$fileSize = $_FILES['file']['size']; //size of the uploaded file in bytes.
+			$fileType = $_FILES['file']['type']; //Multipurpose Internet Mail Extensions(MIME)type of the uploaded file.
+			$fileNameCmps = explode(".", $fileName); //an array created by exploding the $fileName on the "." character. This is done to get the extension of the file.
+			$fileExtension = strtolower(end($fileNameCmps)); //extension of the uploaded file in lowercase. 	
+			$uploadFileDir = './uploads/'; //directory where the file will be uploaded.
+			$dest_path = $uploadFileDir . $fileName; //set to the full path of the uploaded file, which is a combination of $uploadFileDir and $fileName.
+			$target_file = $uploadFileDir.basename($fileName); // set to the basename of the uploaded file, which is the same as $fileName.
+			$allowedFileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'pdf', 'docx', 'jpeg', 'doc');
+			
+			if (in_array($fileExtension, $allowedFileExtensions))
+			{
+				if(file_exists($target_file))
+				{
+				$response->status = "error";
+				$response->message = "File already exist";
+				}
+				elseif($fileSize>50000000)
+				{
+				$response->status = "error";
+				$response->message  =  "File is too large to upload; size limit is 50MB";
+				}       
+				elseif(move_uploaded_file($fileTmpPath, $dest_path))
+				{
+				$response->status = "success";
+				$response->message  = "File is successfully uploaded";
+				}
+				else
+				{
+				$response->status = "error";
+				$response->message  = "Please make sure that the upload directory is writable by web server";
+				}
+			}
+			else
+			{
+				$response->status = "error";
+				$response->message  = "Please upload valid extension";
+			}
 		}
+		echo (json_encode($response));
 	}
-  }
-}
+
 	//Fetch_files
 	if($_POST["action"] == "fetchfiles")
 	{
@@ -54,56 +60,67 @@ if (isset($_POST['action']))
 		$fileArr = array();
 		foreach($files as $filename)
 		{
-			array_push($fileArr,array("name"=>basename($filename),"modified"=>date("F d Y H:i:s.",filemtime("uploads/".$filename)),"size"=>formatSizeUnits(filesize("uploads/".$filename))));
+			if($filename != "." && $filename != "..") {
+				array_push($fileArr,array("name"=>basename($filename),"modified"=>date("F d Y H:i:s",filemtime("uploads/".$filename)),"size"=>formatSizeUnits(filesize("uploads/".$filename))));
+			}
 		}
-		echo json_encode($fileArr);
+		echo (json_encode($fileArr));
 	}
-	
+		
 	//Update_file_names
 	if($_POST["action"] == "update")
-    {	
+	{	
 		$uploadFileDir = 'uploads/';
 		$oldName = $uploadFileDir."/".$_POST["fname"];
 		$newName = $uploadFileDir."/".$_POST["newname"];
 		$oldNameCmps = explode(".",$_POST["fname"] );
-        $oldFileExtension = strtolower(end($oldNameCmps));
+		$oldFileExtension = strtolower(end($oldNameCmps));
 		$newNameCmps = explode(".", $_POST["newname"]);
-        $newFileExtension = strtolower(end($newNameCmps));	
-		if($oldName == $newName) 
+		$newFileExtension = strtolower(end($newNameCmps));
+		
+		if(file_exists($newName) && $oldName != $newName) 
 		{
-			http_response_code(400);
-			echo json_encode(array("message" => "Old and new file name are the same."));
-        }
-		else if(file_exists($newName)) 
-		{
-			http_response_code(400);
-			echo json_encode(array("message" => "File already exists."));
+			$response->status = "error";
+			$response->message = "File already exist with the same name";
 		}
 		else if($oldFileExtension != $newFileExtension)
 		{
-			http_response_code(400);
-			echo json_encode(array("message" => "File extensions can't be renamed."));
+			$response->status = "error";
+			$response->message = "File extensions can't be renamed";
 		}
 		else if(rename($oldName, $newName)) 
 		{
-			http_response_code(200);
-			echo json_encode(array("message" => "File successfully updated."));
+			$response->status = "success";
+			$response->message = "File successfully updated";
 		}
-    }
-	
+		echo (json_encode($response));
+	}
+		
 	//Delete_files
 	if($_POST["action"] == "delete")
 	{
 		$uploadFileDir = 'uploads/'; 
 		if (file_exists($uploadFileDir)) 
 		{
-		  unlink($uploadFileDir.$_POST['file']); 
-		  echo json_encode(array("result" => "success"));
-		} 
-		else 
-		{ 
-		   echo json_encode(array("result" => "failure"));	   
-		} 
+			unlink($uploadFileDir.$_POST['file']);
+			$response->status = "success"; 
+			$response->message = "File successfully deleted";
+		}
+		echo json_encode($response); 
+	}
+
+	//Download_files
+	if($_POST["action"] == "download") {
+		$filename = $_POST['filename'];
+		$extension = $_POST['extension'];
+		$filepath = 'uploads/' . $filename;
+	
+		if (file_exists($filepath)) {
+			header('Content-Type: application/octet-stream');
+			header('Content-Disposition: attachment; filename=' . $filename);
+			header('Content-Length: ' . filesize($filepath));
+			echo file_get_contents($filepath);
+		}
 	}
 	
 	//File_size_caluculation
